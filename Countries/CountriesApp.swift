0df41efab92554531993cba28f193eec8c2efc9d
Lou Allen Uy Sison <louallen@Allens-MacBook-Pro.local>
2021-11-27 19:49:28 +0800

    Initial Commit

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Lou Allen Uy Sison on 11/27/21.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
